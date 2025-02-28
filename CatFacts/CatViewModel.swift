c833e0ca222d67bf53d62d685821b046700440e8
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-28 00:49:40 -0300

    14. Load All button - Add a "Load All" button to the bottom left of the bottom bar - Create a loadAll function in the CatViewModel that correctly uses recursion to repeatedly call getData until the last page of cat data is loaded. The .status area of the toolbar should update to reflect that all cats have been downloaded (e.g. "98 of 98 breeds") 15.Progress View - Add a Progress View (same as we usually do – red, scaled 4 times, centered in the View) that will show when data is loading, but turn oW when data is no longer loading.

--- CatFacts/CatViewModel.swift
+++ CatFacts/CatViewModel.swift
@@ -18,16 +18,18 @@ class CatViewModel {
     
     var breeds = [CatBreed]()
     var total = 0
-    
     var urlString = "https://catfact.ninja/breeds"
+    var isLoading = false
     
     
     func getData() async {
+        isLoading = true
         print("🕸️Accesing to the Web. url: \(urlString)")
         
         // Create URL
         guard let url = URL(string: urlString) else {
             print("😡URL ERROR: We couldn't convert the string to a valid URL, chefck the urlString.")
+            isLoading = false
             return
         }
         
@@ -37,6 +39,7 @@ class CatViewModel {
             // Try to decode JSON data into our own data structures
             guard let returned = try?JSONDecoder().decode(Returned.self, from: data) else {
                 print("😡JSON ERROR: Could not decode returned JSON data.")
+                isLoading = false
                 return
             }
             
@@ -44,10 +47,12 @@ class CatViewModel {
                 urlString = returned.next_page_url ?? ""
                 breeds += returned.data // Make sure you add nuew page to existing page of breeds
                 total = returned.total
+                isLoading = false
             }
             print("😎 Breeds returned! Total #: \(breeds.count)")
         } catch {
             print("😡SOMETHING WENT WRONG: Could not get data from urlString\n\(error.localizedDescription).")
+            isLoading = false
         }
     }
     
@@ -59,8 +64,13 @@ class CatViewModel {
             Task {
                 await getData()
             }
-//        } else {
-            
         }
+    }
+    
+    
+    func loadAll() async {
+        guard urlString != "" else { return } // Don't try to load if you have already loaded the last page of JSON
+        await getData() // get next page of data
+        await loadAll() // call loadAll again - recursion will stop when all pages are retrieved
     }
 }
--- CatFacts/ListView.swift
+++ CatFacts/ListView.swift
@@ -12,25 +12,41 @@ struct ListView: View {
     
     var body: some View {
         NavigationStack {
-            List(catVM.breeds) { catBreed in
-                LazyVStack(alignment: .leading) {
-                    NavigationLink {
-                        DetailView(catBreed: catBreed)
-                    } label: {
-                        Text(catBreed.breed)
-                            .font(.title2)
+            ZStack {
+                List(catVM.breeds) { catBreed in
+                    LazyVStack(alignment: .leading) {
+                        NavigationLink {
+                            DetailView(catBreed: catBreed)
+                        } label: {
+                            Text(catBreed.breed)
+                                .font(.title2)
+                        }
+                    }
+                    .task {
+                        await catVM.loadNextIfNeeded(catBreed: catBreed)
+                    }
+                    
+                }
+                .listStyle(.plain)
+                .navigationTitle("Cat Breeds:")
+                .toolbar {
+                    ToolbarItem(placement: .status) {
+                        Text("\(catVM.breeds.count) of \(catVM.total) breeds.")
+                    }
+                    
+                    ToolbarItem(placement: .bottomBar) {
+                        Button("Load All") {
+                            Task {
+                                await catVM.loadAll()
+                            }
+                        }
                     }
                 }
-                .task {
-                    await catVM.loadNextIfNeeded(catBreed: catBreed)
-                }
-
-            }
-            .listStyle(.plain)
-            .navigationTitle("Cat Breeds:")
-            .toolbar {
-                ToolbarItem(placement: .status) {
-                    Text("\(catVM.breeds.count) of \(catVM.total) breeds.")
+                
+                if catVM.isLoading {
+                    ProgressView()
+                        .scaleEffect(4)
+                        .tint(.red)
                 }
             }
         }
