d6d127d5ff7ef9fcbb08c94fe1008e49d8d3754f
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-27 22:47:08 -0300

    Initial Commit

---
+++ CatFacts/CatFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsApp.swift
+//  CatFacts
+//
+//  Created by Christian Manzaraz on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
