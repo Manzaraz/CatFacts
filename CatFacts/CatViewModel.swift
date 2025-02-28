ea245f277e764c4c0e26dbc64b2b36bfff16d77c
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-27 23:22:02 -0300

    5. Create a CatViewModel. This class should:
    9
    
    - Store the URL used to access the API in a String property named
    urlString, initialized to:
    https://catfact.ninja/breeds
    - Have a property named breeds which is an array of CatBreed
    structs, and which will hold data returned by the data array by
    the above URL.
    - Create a function called getData that will do whatever is
    necessary to access the API at the URL in urlString, and return
    the data, assigning this to the breeds properties of the
    CatViewModel class.
    Hint: you may want to simply access a single variable to make sure that you can get data, then
    expand your work to make sure you can get the array of data that you need.
    - Do whatever else is necessary for this to act like a ViewModel that can access data from an API,
    parse the data into the breeds

---
+++ CatFacts/CatViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  CatViewModel.swift
+//  CatFacts
+//
+//  Created by Christian Manzaraz on 27/02/2025.
+//
+
+import Foundation
+
+@Observable
+class CatViewModel {
+    
+    struct Returned: Codable {
+        var data: [CatBreed]
+    }
+    
+    var breeds = [CatBreed]()
+    
+    var urlString = "https://catfact.ninja/breeds"
+    
+    
+    func getData() async {
+        print("🕸️Accesing to the Web. url: \(urlString)")
+        
+        // Create URL
+        guard let url = URL(string: urlString) else {
+            print("😡URL ERROR: We couldn't convert the string to a valid URL, chefck the urlString.")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // Try to decode JSON data into our own data structures
+            guard let returned = try?JSONDecoder().decode(Returned.self, from: data) else {
+                print("😡JSON ERROR: Could not decode returned JSON data.")
+                return
+            }
+            
+            Task {@MainActor in
+                breeds = returned.data
+            }
+            print("😎 Breeds returned! Total #: \(breeds.count)")
+        } catch {
+            print("😡SOMETHING WENT WRONG: Could not get data from urlString\n\(error.localizedDescription).")
+        }
+    }
+}
